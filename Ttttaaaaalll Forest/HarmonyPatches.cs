60160ff19a21bbe2737ef75c60958fb7b20eff4c
ColossusYTTV <91232680+ColossusYTTV@users.noreply.github.com>
2022-10-08 19:21:41 -0500

    Add files via upload

---
+++ Ttttaaaaalll Forest/HarmonyPatches.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+
+namespace Colossal
+{
+	public class HarmonyPatches
+	{
+		public static bool IsPatched { get; private set; }
+
+		internal static void ApplyHarmonyPatches()
+		{
+			bool flag = !HarmonyPatches.IsPatched;
+			bool flag2 = flag;
+			if (flag2)
+			{
+				bool flag3 = HarmonyPatches.instance == null;
+				bool flag4 = flag3;
+				if (flag4)
+				{
+					HarmonyPatches.instance = new Harmony("org.Colossal");
+				}
+				HarmonyPatches.instance.PatchAll(Assembly.GetExecutingAssembly());
+				HarmonyPatches.IsPatched = true;
+			}
+		}
+
+		internal static void RemoveHarmonyPatches()
+		{
+			bool flag = HarmonyPatches.instance != null && HarmonyPatches.IsPatched;
+			bool flag2 = flag;
+			if (flag2)
+			{
+				HarmonyPatches.instance.UnpatchSelf();
+				HarmonyPatches.IsPatched = false;
+			}
+		}
+
+		private static Harmony instance;
+
+		public const string InstanceId = "org.Colossal";
+	}
+}
---
+++ Ttttaaaaalll Forest/Main.cs
@@ -0,0 +1,41 @@
+﻿
+using BepInEx;
+using UnityEngine;
+
+namespace Colossal
+{
+    [BepInPlugin("org.ColossusYTTV", "2DMonk", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        private void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        private void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        public bool Done;
+        private GameObject KickTrigger;
+        private void Update()
+        {
+            if (!Done)
+            {
+                Done = true;
+
+                GameObject monk = GameObject.Find("Player");
+                monk.transform.position = new Vector3(0.2f, 1, -5);
+
+                KickTrigger = GameObject.Find("QuitBox");
+                KickTrigger.SetActive(false);
+                GameObject.Find("Level").transform.localScale = new Vector3(1f, 50, 1);
+            }
+            else
+            {
+                Done = false;
+            }
+        }
+    }
+}
